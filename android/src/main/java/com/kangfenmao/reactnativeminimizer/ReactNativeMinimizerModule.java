f735427d4d60608bf7cfb2ad7bc03e851361ffd4
kangfenmao <kangfenmao@qq.com>
2023-03-10 09:21:52 +0800

    feat(scheme): added uri protocol minimizer to android project

--- android/src/main/java/com/kangfenmao/reactnativeminimizer/ReactNativeMinimizerModule.java
+++ android/src/main/java/com/kangfenmao/reactnativeminimizer/ReactNativeMinimizerModule.java
@@ -1,44 +1,48 @@
 package com.kangfenmao.reactnativeminimizer;
 
+import androidx.annotation.NonNull;
+
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
-import com.facebook.react.bridge.Callback;
+import com.facebook.react.module.annotations.ReactModule;
 import android.app.Activity;
 import android.content.Intent;
 
-public class MinimizerModule extends ReactContextBaseJavaModule {
+@ReactModule(name = ReactNativeMinimizerModule.NAME)
+public class ReactNativeMinimizerModule extends ReactContextBaseJavaModule {
+  public static final String NAME = "ReactNativeMinimizer";
+  private ReactApplicationContext reactContext;
 
-    private final ReactApplicationContext reactContext;
+  public ReactNativeMinimizerModule(ReactApplicationContext reactContext) {
+    super(reactContext);
+    this.reactContext = reactContext;
+  }
 
-    public MinimizerModule(ReactApplicationContext reactContext) {
-        super(reactContext);
-        this.reactContext = reactContext;
-    }
+  @Override
+  @NonNull
+  public String getName() {
+    return NAME;
+  }
 
-    @Override
-    public String getName() {
-        return "Minimizer";
-    }
+  @ReactMethod
+  public void exit() {
+    Activity activity = this.reactContext.getCurrentActivity();
+    activity.finishAffinity();
+    System.exit(0);
+  }
 
-    @ReactMethod
-    public void exit() {
-      Activity activity = reactContext.getCurrentActivity();
-      activity.finishAffinity();
-      System.exit(0);
-    }
+  @ReactMethod
+  public void goBack() {
+    Activity activity = this.reactContext.getCurrentActivity();
+    activity.moveTaskToBack(true);
+  }
 
-    @ReactMethod
-    public void goBack() {
-      Activity activity = reactContext.getCurrentActivity();
-      activity.moveTaskToBack(true);
-    }
-
-    @ReactMethod
-    public void minimize() {
-      Intent startMain = new Intent(Intent.ACTION_MAIN);
-      startMain.addCategory(Intent.CATEGORY_HOME);
-      startMain.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-      this.reactContext.startActivity(startMain);
-    }
+  @ReactMethod
+  public void minimize() {
+    Intent startMain = new Intent(Intent.ACTION_MAIN);
+    startMain.addCategory(Intent.CATEGORY_HOME);
+    startMain.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+    this.reactContext.startActivity(startMain);
+  }
 }
