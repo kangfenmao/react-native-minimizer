2f1ff29875c8f79574f35157ca47b1e04b72bb0a
kangfenmao <kangfenmao@qq.com>
2023-03-17 12:55:32 +0800

    fix(electron): import typeof AccessibilityInfo from ... error

--- src/index.tsx
+++ src/index.tsx
@@ -1,39 +1,30 @@
-import { NativeModules, Platform } from 'react-native';
-
-const LINKING_ERROR =
-  `The package '@kangfenmao/react-native-minimizer' doesn't seem to be linked. Make sure: \n\n` +
-  Platform.select({ ios: "- You have run 'pod install'\n", default: '' }) +
-  '- You rebuilt the app after installing the package\n' +
-  '- You are not using Expo Go\n';
-
-const ReactNativeMinimizer = NativeModules.ReactNativeMinimizer
-  ? NativeModules.ReactNativeMinimizer
-  : new Proxy(
-      {},
-      {
-        get() {
-          throw new Error(LINKING_ERROR);
-        },
-      }
-    );
-
-export const minimize = (): void => {
-  console.log('minimize');
-  ReactNativeMinimizer.minimize();
+let Minimizer = {
+  minimize: () => {},
+  goBack: () => {},
+  exit: () => {},
 };
 
-export const goBack = (): void => {
-  console.log('goBack');
-  ReactNativeMinimizer.goBack();
-};
+if (!process.env.ELECTRON) {
+  const { NativeModules, Platform } = require('react-native');
 
-export const exit = (): void => {
-  console.log('exit');
-  ReactNativeMinimizer.exit();
-};
+  const LINKING_ERROR =
+    `The package '@kangfenmao/react-native-minimizer' doesn't seem to be linked. Make sure: \n\n` +
+    Platform.select({ ios: "- You have run 'pod install'\n", default: '' }) +
+    '- You rebuilt the app after installing the package\n' +
+    '- You are not using Expo Go\n';
 
-export default {
-  minimize,
-  goBack,
-  exit,
-};
+  const ReactNativeMinimizer = NativeModules.ReactNativeMinimizer
+    ? NativeModules.ReactNativeMinimizer
+    : new Proxy(
+        {},
+        {
+          get() {
+            throw new Error(LINKING_ERROR);
+          },
+        }
+      );
+
+  Minimizer = ReactNativeMinimizer;
+}
+
+export default Minimizer;
