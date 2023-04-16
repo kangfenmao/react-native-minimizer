2b4b5968dd51d63d3bd7db3e0bb9f8bedd2b7066
kangfenmao <kangfenmao@qq.com>
2023-04-16 12:38:00 +0800

    Revert "fix(electron): import typeof AccessibilityInfo from ... error"
    This reverts commit 2f1ff29875c8f79574f35157ca47b1e04b72bb0a.

--- src/index.tsx
+++ src/index.tsx
@@ -1,30 +1,39 @@
-let Minimizer = {
-  minimize: () => {},
-  goBack: () => {},
-  exit: () => {},
+import { NativeModules, Platform } from 'react-native';
+
+const LINKING_ERROR =
+  `The package '@kangfenmao/react-native-minimizer' doesn't seem to be linked. Make sure: \n\n` +
+  Platform.select({ ios: "- You have run 'pod install'\n", default: '' }) +
+  '- You rebuilt the app after installing the package\n' +
+  '- You are not using Expo Go\n';
+
+const ReactNativeMinimizer = NativeModules.ReactNativeMinimizer
+  ? NativeModules.ReactNativeMinimizer
+  : new Proxy(
+      {},
+      {
+        get() {
+          throw new Error(LINKING_ERROR);
+        },
+      }
+    );
+
+export const minimize = (): void => {
+  console.log('minimize');
+  ReactNativeMinimizer.minimize();
 };
 
-if (!process.env.ELECTRON) {
-  const { NativeModules, Platform } = require('react-native');
+export const goBack = (): void => {
+  console.log('goBack');
+  ReactNativeMinimizer.goBack();
+};
 
-  const LINKING_ERROR =
-    `The package '@kangfenmao/react-native-minimizer' doesn't seem to be linked. Make sure: \n\n` +
-    Platform.select({ ios: "- You have run 'pod install'\n", default: '' }) +
-    '- You rebuilt the app after installing the package\n' +
-    '- You are not using Expo Go\n';
+export const exit = (): void => {
+  console.log('exit');
+  ReactNativeMinimizer.exit();
+};
 
-  const ReactNativeMinimizer = NativeModules.ReactNativeMinimizer
-    ? NativeModules.ReactNativeMinimizer
-    : new Proxy(
-        {},
-        {
-          get() {
-            throw new Error(LINKING_ERROR);
-          },
-        }
-      );
-
-  Minimizer = ReactNativeMinimizer;
-}
-
-export default Minimizer;
+export default {
+  minimize,
+  goBack,
+  exit,
+};
